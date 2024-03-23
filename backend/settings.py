f3f693cbfcd11f5f78e3e0c3893a2e48e2c66e5f
Akash Ps <itzmeakashps@gmail.com>
2024-03-23 13:09:06 +0530

    some changes in settings

--- backend/settings.py
+++ backend/settings.py
@@ -15,17 +15,20 @@ import os
 from datetime import timedelta
 import dj_database_url
 from decouple import config
-
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file =  os.path.join(BASE_DIR, '.env')
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+    
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ['SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "False"
 
